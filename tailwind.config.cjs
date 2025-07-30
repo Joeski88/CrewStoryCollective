30fe43587ab6196f94311271ba3bc363708ddc34
Joeski88 <joe.howley88@gmail.com>
2025-07-30 17:20:47 +0100

    trying to debug the styling issues with tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+          sage: {
+            100: '#E6E4D8',
+            500: '#B2AC88',
+            800: '#7A7551',
+          },
+          olive: {
+            100: '#E2E6D4',
+            500: '#708238',
+            800: '#4C5A27',
+          },
+          terracotta: {
+            100: '#FBEDE9',
+            500: '#E2725B',
+            800: '#C2543C',
+          },
+          midnight: {
+            100: '#2E2F36',
+            500: '#1E1F26',
+            800: '#121318',
+          },
+          cream: {
+            100: '#FFFDF4',
+            500: '#FFFDD0',
+            800: '#E6E4AF',
+          },
+        },
+
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        heading: ['"Playfair Display"', 'serif'],
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem',
+      },
+    },
   },
   plugins: [],
 }
